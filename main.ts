314b52d80521573d4cdfc50b0135325431e0140d
Darren Grants <darrengrants06@gmail.com>
2024-12-07 19:05:31 +0000

    root files, changed?

--- main.ts
+++ main.ts
@@ -3,7 +3,7 @@
  *  Protractor is used in this example for compatibility with Angular documentation tools.
  */
 import {bootstrapApplication, provideProtractorTestingSupport} from '@angular/platform-browser';
-import {AppComponent} from './app/app.component';
+import {AppComponent} from './app.component';
 
 bootstrapApplication(AppComponent, {providers: [provideProtractorTestingSupport()]}).catch((err) =>
   console.error(err),
